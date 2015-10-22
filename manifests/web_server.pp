71f93c59a9f230f659d38945c4573d322abfcb7f
James Forrest <james@forrestclan.com>
2015-10-22 00:48:32 +0000

    added debug for hiera lookups

--- manifests/web_server.pp
+++ manifests/web_server.pp
@@ -1,7 +1,11 @@
 class profiles::web_server {
 
+	$root_pwd = hiera('mysql::server::root_password')
+	notify { "the root password is ${root_pwd}": }
+
+
 	class { '::mysql::server':
-		root_password        => hiera('mysql::server::root_password'),
+		root_password        => $root_pwd,
 		remove_default_accounts => true,
 	}
 
